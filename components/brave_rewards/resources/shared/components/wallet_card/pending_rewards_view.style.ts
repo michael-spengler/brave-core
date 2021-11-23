09343b47d1011bc84630c35a80e759f203302383
zenparsing <zenparsing@gmail.com>
2021-11-23 16:01:58 -0500

    Update Rewards payout status messaging

--- components/brave_rewards/resources/shared/components/wallet_card/pending_rewards_view.style.ts
+++ components/brave_rewards/resources/shared/components/wallet_card/pending_rewards_view.style.ts
@@ -1,40 +1,69 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
-* License, v. 2.0. If a copy of the MPL was not distributed with this file,
-* You can obtain one at http://mozilla.org/MPL/2.0/. */
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import styled from 'styled-components'
 
-export const root = styled.div``
-
-export const pendingRewards = styled.div`
-  background: var(--brave-palette-grey900);
-  color: var(--brave-palette-grey400);
-
-  .brave-theme-dark & {
-    background: rgba(30, 32, 41, .8);
-  }
-
-  font-size: 12px;
-  line-height: 22px;
-  text-align: center;
-  padding: 8px;
-
-  .icon {
-    color: var(--brave-palette-yellow500);
-    height: 16px;
-    width: auto;
-    vertical-align: middle;
-    margin-right: 7px;
-    margin-bottom: 3px;
-  }
-`
-
-export const pendingAmount = styled.span`
-  color: var(--brave-palette-grey000);
+export const root = styled.div`
+  color: var(--brave-palette-neutral900);
   font-size: 14px;
-  line-height: 24px;
+  line-height: 21px;
+  text-align: left;
 
-  .plus {
-    margin-right: 2px;
+  > div {
+    background: #E8F4FF;
+
+    .brave-theme-dark & {
+      background: #DFF0FE;
+    }
+  }
+
+  a {
+    color: var(--brave-palette-blurple500);
+    font-weight: 600;
+    text-decoration: none;
+  }
+
+  .rewards-payment-pending {
+    padding: 7px 24px;
+    display: flex;
+    gap: 8px;
+
+    .icon {
+      color: var(--brave-palette-blue500);
+      height: 14px;
+      width: auto;
+      vertical-align: middle;
+    }
+  }
+
+  .rewards-payment-amount {
+    font-weight: 600;
+
+    .plus {
+      margin-right: 2px;
+    }
+  }
+
+  .rewards-payment-completed {
+    background: #E7FDEA;
+    padding: 10px 15px;
+    display: flex;
+    align-items: center;
+    gap: 13px;
+
+    .brave-theme-dark & {
+      background: #CBF1D1;
+    }
+
+    .icon {
+      height: 24px;
+      width: auto;
+      vertical-align: middle;
+    }
+  }
+
+  .rewards-payment-processing {
+    padding: 7px 24px;
   }
 `
